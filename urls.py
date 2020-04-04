4984f074870d44ca426a02352fed4626af9172f6
Khaled Boka <bokae97@gmail.com>
2020-04-05 01:13:15 +0200

    fixed: url patterns django 1.11 compatible

--- urls.py
+++ urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from . import APP_NAME, views
 
-urlpatterns = patterns('',
-                       url(r'^new/$', views.new,
-                           name='%s.new' % APP_NAME),
-                       url(r'^(?P<instance_id>\d+)/edit/$',
-                           views.edit, name='%s.edit' % APP_NAME),
-                       url(r'^(?P<instance_id>\d+)/view/$',
-                           views.view_app, name='%s.view' % APP_NAME)
-                       )
+urlpatterns = [
+    url(r'^new/$', views.new,
+        name='%s.new' % APP_NAME),
+    url(r'^(?P<instance_id>\d+)/edit/$',
+        views.edit, name='%s.edit' % APP_NAME),
+    url(r'^(?P<instance_id>\d+)/view/$',
+        views.view_app, name='%s.view' % APP_NAME)
+]
